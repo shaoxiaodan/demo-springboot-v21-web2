d2c2c28db56dc7d0f767f4385e719c9732572911
shaoxiaodan <sandays_cn@163.com>
2024-08-24 12:50:58 -0700

    创建springboot-v21项目代码

---
+++ src/main/java/com/example/demo/DemoSpringbootV21Web2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootV21Web2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootV21Web2Application.class, args);
+	}
+
+}
