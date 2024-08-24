ca4388fd00f69ebc67986c7543c4f4873068fc59
shaoxiaodan <sandays_cn@163.com>
2024-08-24 14:56:52 -0700

    Update MyLocaleResolver.java

--- src/main/java/com/example/demo/config/MyLocaleResolver.java
+++ src/main/java/com/example/demo/config/MyLocaleResolver.java
@@ -1,8 +1,29 @@
 package com.example.demo.config;
 
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.LocaleResolver;
+
 /*
  * 自定义国际化解析器
  */
-public class MyLocaleResolver {
+public class MyLocaleResolver implements LocaleResolver {
+
+	//解析请求，获取语言信息
+	@Override
+	public Locale resolveLocale(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// 设置语言
+	@Override
+	public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
