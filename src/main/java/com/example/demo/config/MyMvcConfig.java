12b3faff7117dc75017c1c7bd160ed4ba321f733
shaoxiaodan <sandays_cn@163.com>
2024-08-24 21:34:16 -0700

    注册并配置自定义拦截器

--- src/main/java/com/example/demo/config/MyMvcConfig.java
+++ src/main/java/com/example/demo/config/MyMvcConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,13 +15,13 @@ public class MyMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 
-		// 登录
-		registry.addViewController("/login").setViewName("sign-in");
-		registry.addViewController("/login.html").setViewName("sign-in");
+		/*
+		 * 配置视图跳转和html之间的映射
+		 */
+		registry.addViewController("/").setViewName("index"); // 主页
+		registry.addViewController("/index.html").setViewName("index"); // 主页
+		registry.addViewController("/login.html").setViewName("sign-in"); // 登录
 
-		// 主页
-		registry.addViewController("/").setViewName("index");
-		registry.addViewController("/index.html").setViewName("index");
 	}
 
 	// 注册自定义国际化组件
@@ -28,6 +29,15 @@ public class MyMvcConfig implements WebMvcConfigurer {
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new MyLocaleResolver(); // 返回自定义语言组件
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new MyLoginHandlerInterceptor()) // 注册自定义拦截器
+				.addPathPatterns("/**") // 拦截所有请求
+//				.excludePathPatterns("/", "/index.html", "/login.html"); // 排除其他请求
+				.excludePathPatterns("/login.html") // 排除登录页面
+				.excludePathPatterns("/css/**", "/font/**", "/image/**", "/js/**", "/picture/**"); // 排除静态资源
 	}
 
 }
